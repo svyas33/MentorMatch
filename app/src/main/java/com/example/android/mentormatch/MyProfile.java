6bf0a980e874a574e45e52cbe0d9571bf9159436
ShivaniVyas <shivanivyas33@gmail.com>
2019-03-17 10:26:24 -0400

    connections

--- app/src/main/java/com/example/android/mentormatch/MyProfile.java
+++ app/src/main/java/com/example/android/mentormatch/MyProfile.java
@@ -20,7 +20,6 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
